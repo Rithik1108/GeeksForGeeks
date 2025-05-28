121835eadc8de001db8fed52b432c2c0743192f6
Rithik1108 <rithikreddy567@gmail.com>
2025-05-28 10:38:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Queue using Linked List/queue-using-linked-list.java
@@ -0,0 +1,42 @@
+/*The structure of the node of the queue is
+class QueueNode
+{
+    int data;
+    QueueNode next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = null;
+    }
+}*/
+
+class MyQueue {
+    QueueNode front, rear;
+
+    // Function to push an element into the queue.
+    void push(int a) {
+        // Your code here
+        QueueNode temp = new QueueNode(a);
+        if(rear==null){
+            front=rear=temp;
+        }
+        else{
+            rear.next=temp;
+            rear=temp;
+        }
+    }
+
+    // Function to pop front element from the queue.
+    int pop() {
+        if(front==null){
+            return -1;
+        }
+            int d=front.data;
+            front=front.next;
+            if(front==null){
+                rear=null;
+            }
+        return d;
+        // Your code here
+    }
+}
