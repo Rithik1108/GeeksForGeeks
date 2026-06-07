319c88b752c5a43094625fd217b18446fd010065
Rithik1108 <rithikreddy567@gmail.com>
2026-06-07 21:54:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Number of factors/number-of-factors.java
@@ -0,0 +1,17 @@
+class Solution {
+    static int countFactors(int n) {
+        // code here
+        int count = 0;
+        for(int i=1;i*i<=n;i++){
+            if(n%i==0){
+                if(i==n/i){
+                    count=count+1;
+                }
+                else{
+                    count=count+2;
+                }
+            }
+        }
+        return count;
+    }
+}
