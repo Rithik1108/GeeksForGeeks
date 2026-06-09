9d478ce2d583c812e63b188111dc7739ef24125c
Rithik1108 <rithikreddy567@gmail.com>
2026-06-09 13:18:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Addition of two square matrices/addition-of-two-square-matrices.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+
+class Solution {
+    public void Addition(int[][] matrixA, int[][] matrixB) {
+        // code here
+        for(int i=0;i<matrixA.length;i++){
+            for(int j=0;j<matrixB[0].length;j++){
+                matrixA[i][j] = matrixA[i][j] + matrixB[i][j];
+            }
+        }
+    }
+}
